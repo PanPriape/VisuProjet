6e9b94917939327895c7dc0491fb1fb763982b4a
Durand Vincent <vincent@systemd.net>
2017-06-22 00:46:00 +0200

    Code fonctionnel pour l'export des données normalisées pour tulip vers un fichier texte.

--- dataToTxt.py
+++ dataToTxt.py
@@ -8,22 +8,44 @@
   Acquisition des données utilisées pour modéliser le graphe dans tulip. On doit récupérer la positions de tout les noeuds ainsi que les arretes qui les lient.
 
 """
+### Export des données dans le fichier texte 3DSanguin.txt.
+
 
 import bpy
 
 # obdata contient les données de notre object "Graphe_Art" représentant le flux sanguin
 obdata = bpy.data.objects['Graphe_Art'].data
 
-# permet d'afficher tout les vertices/noeuds de l'objet 3D. Format : 'indexVertice, coordonnéeEnX, coordonnéeEnY, coordonnéeEnZ'
-print('Vertices:')
+# on ouvre le fichier texte
+f=open('/Users/Farfadet/Desktop/Cours - UQAC/trimestre - Ete/Visualisation/projet/blend/3DSanguin.txt','w')
+
+# permet d'écrire tout les vertices/noeuds de l'objet 3D. Format : 'indexVertice, coordonnéeEnX, coordonnéeEnY, coordonnéeEnZ'
+f.write('Vertices :\n')
 for v in obdata.vertices:
-    print('{}. {} {} {}'.format(v.index, v.co.x, v.co.y, v.co.z))
+    f.write('%s %s %s %s\n' % (v.index,v.co[0],v.co[1],v.co[2]))
+
+# permet d'écrire tout les Edges/arretes de l'objet 3D. Format : 'indexEdge, indexVertice1, indexVertice2'
+f.write('Edges :\n')
+for e in obdata.edges:
+    f.write('%s %s %s\n' % (e.index, e.vertices[0], e.vertices[1]))
+
+# fermeture du fichier texte
+f.close()
+
+### Affichage des données dans la console python de Blender.
+
+# obdata contient les données de notre object "Graphe_Art" représentant le flux sanguin
+obdata = bpy.data.objects['Graphe_Art'].data
+
+# permet d'afficher tout les vertices/noeuds de l'objet 3D. Format : 'indexVertice, coordonnéeEnX, coordonnéeEnY, coordonnéeEnZ'
+print('Vertices :\n')
+for v in obdata.vertices:
+    print('{}. {} {} {}'.format(v.index, v.co.x, v.co.y, v.co.z)) 
 
 # permet d'afficher tout les Edges/arretes de l'objet 3D. Format : 'indexEdge, indexVertice1, indexVertice2'
-print('Edges:')
+print('Edges :\n')
 for e in obdata.edges:
     print('{}. {} {}'.format(e.index, e.vertices[0], e.vertices[1]))
-
 
 """
   Code permettant d'inverser le sens/la direction d'une arrete.
@@ -37,6 +59,7 @@ for e in obdata.edges:
         vtmp = e.vertices[0]
         e.vertices[0] = e.vertices[1]
         e.vertices[1] = vtmp
+
 
 """
   Code permettant de récupérer l'index des vertices/noeuds sélectionnés dans Blender(mode Edition).
